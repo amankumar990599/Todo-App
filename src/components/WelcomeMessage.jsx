5574b7b9e203c3748198326b0a6e6b393d872b23
Delta Student <amankumarraj95346@gmail.com>
2025-12-06 16:48:23 +0530

    first commit

---
+++ src/components/WelcomeMessage.jsx
@@ -0,0 +1,6 @@
+import styles from "./WelcomeMessage.module.css";
+
+const WelcomeMessage = () => {
+  return <p className={styles.WelcomeMessageelcome}>Enjoy Your Day</p>;
+};
+export default WelcomeMessage;
